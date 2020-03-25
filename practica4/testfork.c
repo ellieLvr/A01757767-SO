3383f8a8c3f0f45145b3ecdca65d5d015674cd7f
ellieLvr <eleonore.levoir@hei.yncrea.fr>
2020-03-25 18:44:36 +0000

    practica 4

---
+++ practica4/testfork.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+    printf("cuantas veces\n");
+    int pid = fork();
+    if(pid ==0) printf("Soy el hijo\n");
+    else printf("Soy el padre\n");
+    fork();
+    fork();
+    printf("cuantas veces otra vez \n");
+}
